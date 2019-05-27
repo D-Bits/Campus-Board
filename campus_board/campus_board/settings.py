2ca79e18047cb12ea8c5ad8b1eb63b9b0cfe0928
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-05-27 12:41:45 -0700

    updated settings

--- campus_board/campus_board/settings.py
+++ campus_board/campus_board/settings.py
@@ -123,7 +123,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+#
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
