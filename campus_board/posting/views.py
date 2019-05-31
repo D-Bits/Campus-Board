50d9edd84c1ee4438ed5748410f78ff01dea1438
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-05-31 12:58:37 -0700

    minor view changes

--- campus_board/posting/views.py
+++ campus_board/posting/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Author
+from . models import Post, Author
 
 # Home page
 def index(request):
